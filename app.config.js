15d730a727706aed7570b6ea7ef533baf64af2ee
carrot2803 <lezamadmitri@gmail.com>
2024-03-20 01:48:39 -0400

    update to expo sdk 50

--- app.config.js
+++ app.config.js
@@ -33,7 +33,7 @@ export default {
       projectId: process.env.PROJECT_ID,
       storageBucket: process.env.STORAGE_BUCKET,
       messagingSenderId: process.env.MESSAGING_SENDER_ID,
-      appId: process.env.APP_ID
+      appId: process.env.APP_ID,
     }
   }
 }
